308d43e10fa089e8f9eb2cd8f294c261b73ca805
Elchanan5760 <emstone@gmail.com>
2025-05-14 16:54:19 +0300

    decript atbash

--- Program.cs
+++ Program.cs
@@ -1,52 +1,104 @@
-﻿namespace Example
+﻿
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Gets text and decrypt it by atbash.
+static string DecryptText(string text)
 {
-    class AtbashCipher 
+    string newText = "";
+    foreach (char c in text)
     {
-        static void Main()
-        {
-            
-        }
+        char decrypt = ChangeAtbash(char.ToUpper(c));
+        char newChar = char.ToLower(decrypt);
+        newText += newChar;
+    }
+    return newText;
+}
 
-        // Gets text and decrypt it by atbash.
-        static string DecryptText(string text)
-        {
-            string newText = "";
-            foreach (char c in text)
-            {
-                char decrypt = ChangeAtbash(char.ToUpper(c));
-                char newChar = char.ToLower(decrypt);
-                newText += newChar;
-            }
-            return newText;
-        }
+// Gets encrypt char and return it decrypt by atbash.
+static char ChangeAtbash(char c)
+{
+    Dictionary<char, char> atbashDicti = new Dictionary<char, char>
+    {
+        { 'A', 'Z' }, { 'B', 'Y' }, { 'C', 'X' },
+        { 'D', 'W' }, { 'E', 'V' }, { 'F', 'U' },
+        { 'G', 'T' }, { 'H', 'S' }, { 'I', 'R' },
+        { 'J', 'Q' }, { 'K', 'P' }, { 'L', 'O' },
+        { 'M', 'N' }, { 'N', 'M' }, { 'O', 'L' },
+        { 'P', 'K' }, { 'Q', 'J' }, { 'R', 'I' },
+        { 'S', 'H' }, { 'T', 'G' }, { 'U', 'F' },
+        { 'V', 'E' }, { 'W', 'D' }, { 'X', 'C' },
+        { 'Y', 'B' }, { 'Z', 'A' }
+    };
 
-        // Gets encrypt char and return it decrypt by atbash.
-        static char ChangeAtbash(char c)
+    char decryptChar = 'c';
+    foreach (var encryptedLetter in atbashDicti.Keys)
+    {
+        if (encryptedLetter == c)
         {
-            Dictionary<char, char> atbashDicti = new Dictionary<char, char>
-            {
-                { 'A', 'Z' }, { 'B', 'Y' }, { 'C', 'X' },
-                { 'D', 'W' }, { 'E', 'V' }, { 'F', 'U' },
-                { 'G', 'T' }, { 'H', 'S' }, { 'I', 'R' },
-                { 'J', 'Q' }, { 'K', 'P' }, { 'L', 'O' },
-                { 'M', 'N' }, { 'N', 'M' }, { 'O', 'L' },
-                { 'P', 'K' }, { 'Q', 'J' }, { 'R', 'I' },
-                { 'S', 'H' }, { 'T', 'G' }, { 'U', 'F' },
-                { 'V', 'E' }, { 'W', 'D' }, { 'X', 'C' },
-                { 'Y', 'B' }, { 'Z', 'A' }
-            };
-
-            char decryptChar = 'c';
-            foreach (var encryptedLetter in atbashDicti.Keys)
-            {
-                if (encryptedLetter == c)
-                {
-                    decryptChar = atbashDicti[c];
-                }
-            }
-            return decryptChar;
+            decryptChar = atbashDicti[c];
         }
     }
+    return decryptChar;
 }
 
 
